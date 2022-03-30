343b9e7a66c5c4579a0d61724d7e051f64836219
Oliver <oliversotof@gmail.com>
2022-03-30 08:55:43 -0600

    Styled components 	-How to intall 	-Usage 	-Styles 	-=Security

---
+++ styledComponents/src/App.tsx
@@ -0,0 +1,87 @@
+import React from "react"
+import styled, {keyframes} from "styled-components"
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+const Content = styled.div`
+  padding: 20px 25px;
+`
+
+const Button = styled.button<{primary?:boolean}>`
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color : ${props => props.primary ? 'white' : 'red'};
+  padding : 10px 15px;
+  border : solid 2px red;
+  border-radius: 4px;
+
+  &:hover{
+    box-shadow: 1px 2px 5px rgb(0,0,0,0.3)
+  }
+
+  &.secondary{
+    background-color:blue;
+    border : solid 2px blue;
+  }
+`
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`
+
+const Link : React.FC<{className?: any}> = ({ className, ...props}) => {
+  return <a className={className} {...props}></a>
+}
+
+const StyledLink = styled(Link)`
+    color: blue;
+  `
+
+const Input : React.FC<{color?: any}> = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border:1px solid red;
+  color:${props => props.color};
+`
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})``
+
+const girar = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+
+  to { 
+    transform: rotate(360deg)
+  }
+`
+
+const Rotar = styled.div`
+  animation: ${girar} 2s linear infinite;
+`
+
+function App() {
+  return (
+    <Content>
+      <P>Hola soy un parrafo</P>
+      <Button>Enviar</Button>
+      <Button className="secondary">Enviar</Button>
+      <BlockButton primary as="a" href="#">Enviar</BlockButton>
+      <BlockButton primary>Enviar</BlockButton>
+      <Link >Link</Link>
+      <StyledLink>Link con estilo</StyledLink>
+      <Input color="blue"/>
+      <Password/>
+      <br />
+      <Rotar> Estor Girando</Rotar>
+    </Content>
+  )
+}
+
+export default App
